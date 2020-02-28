948eae49f8301cc9afbeb9c0bdd1fa1025f18dde
Ivanchia0909 <48830330+Ivanchia0909@users.noreply.github.com>
2020-02-28 11:50:02 +0800

    Added leveling and exp bar
    lvl and exp bar

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 /*
     Script: Player
@@ -11,42 +12,71 @@ public class Player : MonoBehaviour
 {
     // Properties
     public float health = 10f;                      // Total amount of health the player has.
-    public int score;                               // This player's score.
+    public int score;
+    public int maxScore = 5;
+    // This player's score.
+    public ExpBar expBar;
+    int level;
+    public Text currentLevel;
 
+
+
+
+
+    void Start()
+    {
+        score = 0;
+        expBar.SetZeroExp(score);
+        level = 0;
+        currentLevel.text = "Level : 0";
+
+    }
     // Methods
-    public void TakeDamage( float amountOfDamage )
+    public void TakeDamage(float amountOfDamage)
     {
         // Don't take damage if health already zero.
-        if( this.health == 0f ) { return; }
+        if (this.health == 0f) { return; }
 
         // Apply damage.
         this.health -= amountOfDamage;
-        if( this.health < 0f ) { this.health = 0f; }
+        if (this.health < 0f) { this.health = 0f; }
 
         // Check if health just became zero.
-        if( this.health == 0f )
+        if (this.health == 0f)
         {
-            Debug.Log( "Player killed." );
+            Debug.Log("Player killed.");
 
             // Disable PlayerController input.
             PlayerController[] playerControllers = GameObject.FindObjectsOfType<PlayerController>();
-            foreach( PlayerController pController in playerControllers ){ pController.enabled = false; }
+            foreach (PlayerController pController in playerControllers) { pController.enabled = false; }
         }
     }
 
-    private void OnTriggerEnter( Collider collider ) // This method automatically gets called when the object it is on enters a collider (Set to trigger)
+    private void OnTriggerEnter(Collider collider) // This method automatically gets called when the object it is on enters a collider (Set to trigger)
     {
         // Sanity check.
-        if( collider == null ){ return; }
+        if (collider == null) { return; }
 
         // Check to see if the collider game object has a Pickup component.
         Pickup pickup = collider.gameObject.GetComponent<Pickup>();
-        if( pickup != null )
+        if (pickup != null)
         {
             this.score += pickup.scoreValue;
+            expBar.SetExp(score);
+            if (score >= maxScore)
+            {
+                
+                expBar.SetZeroExp(score);
+                level += 1;
+                currentLevel.text = "Level : " + level.ToString();
+                
+
+            }
 
             // Destroy the pickup.
-            Destroy( collider.gameObject );
+            Destroy(collider.gameObject);
         }
     }
+
+
 }
